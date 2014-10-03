c6b4947a1745ad87706f830563a2b910277b4a63
Thibault Clérice <leponteineptique@gmail.com>
2014-10-03 17:05:34 +0200

    Lines changes...

--- Tests/Linguistic.Contextualiser.Sentence.py
+++ Tests/Linguistic.Contextualiser.Sentence.py
@@ -9,10 +9,12 @@ from Data import Models
 import Services.Perseus
 import Linguistic
 
+
 Perseus = Services.Perseus.MySQL
 Lemma = Services.Perseus.MySQL.Lemma
 Contextualiser = Linguistic.Contextualiser.WordWindow
 Occurence = Services.Perseus.MySQL.Occurence
+
 
 #Set up with real data
 L = Lemma()
