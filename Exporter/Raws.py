8affbdc48a5363b719633c018ab358df11844706
Thibault Clérice <leponteineptique@gmail.com>
2014-10-06 18:19:05 +0200

    First exporter

--- Exporter/Raws.py
+++ Exporter/Raws.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import codecs
+import time
+import sys
+
+sys.path.append("../..")
+
+
 from Data.Models import *
+from Exporter.common import model
+
+
+class RCorpus(object):
+	def __init__(self, OccurenceSetList, folder = "./"):
+		self.OccurenceSetList = OccurenceSetList
+		self.folder = folder
+
+	def write(self):
+		for OccurenceSet in self.OccurenceSetList:
+			with codecs.open("{0}data/corpus/{1}-{2}.txt".format(self.folder, self.OccurenceSet.lemma.toString(), time.strftime("%Y-%m-%d.%H-%M")), "w") as f:
+				f.write(" ".join([Occurence.lemmatizedToString() for Occurence in OccurenceSet.occurences]))
+				f.close()
+		return True
